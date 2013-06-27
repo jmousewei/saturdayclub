bb12c85d0a180ac17fc8f876ae9d616b8ffdadac
jmousewei <cwei1914@hotmail.com>
2013-06-27 18:41:13 +0800

    Create test version

---
+++ saturdayclub/Controllers/MsgController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace saturdayclub.Controllers
+{
+    public class MsgController : Controller
+    {
+        //
+        // GET: /Msg/
+
+        [ActionName("valid")]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Valid()
+        {
+            return View("Default.aspx");
+        }
+
+    }
+}
---
+++ saturdayclub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="saturdayclub.MvcApplication" Language="C#" %>
