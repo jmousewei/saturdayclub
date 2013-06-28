f3d11138e70abdd19e2f9a5bbfa66a3e22a50803
jmousewei <cwei1914@hotmail.com>
2013-06-28 10:47:09 +0800

    Complete valid procedure

--- saturdayclub/Controllers/MsgController.cs
+++ saturdayclub/Controllers/MsgController.cs
@@ -3,19 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace saturdayclub.Controllers
 {
     public class MsgController : Controller
     {
+        public static readonly string MessageToken = "saturdayclub";
+
         //
         // GET: /Msg/
 
         [ActionName("valid")]
         [AcceptVerbs(HttpVerbs.Get)]
-        public ActionResult Valid()
+        public ActionResult Valid(string signature, string timestamp, string nonce, string echostr)
         {
-            return View("Default.aspx");
+            ActionResult result = Content("Please visit via your WeiXin client.");
+            do
+            {
+                if (string.IsNullOrEmpty(signature) ||
+                    string.IsNullOrEmpty(timestamp) ||
+                    string.IsNullOrEmpty(nonce) ||
+                    string.IsNullOrEmpty(echostr))
+                {
+                    break;
+                }
+
+                string[] param = new[] { MessageToken, timestamp, nonce };
+                Array.Sort<string>(param);
+                string hashParam = string.Join(string.Empty, param);
+                byte[] buf = Encoding.UTF8.GetBytes(hashParam);
+                using (var sha1 = SHA1.Create())
+                {
+                    var hash = sha1.ComputeHash(buf);
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var b in hash)
+                    {
+                        if (b < 0x10)
+                            sb.Append('0');
+                        sb.AppendFormat("{0:x}", b);
+                    }
+                    string hashStr = sb.ToString();
+                    if (string.Compare(signature, hashStr, true) == 0)
+                    {
+                        result = Content(echostr);
+                    }
+                }
+
+            } while (false);
+            return result;
         }
 
     }
