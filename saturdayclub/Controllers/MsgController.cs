387575a78e2524b9cb6178f4ec108a33f76b4c39
jmousewei <cwei1914@hotmail.com>
2013-06-30 16:37:48 +0800

    add state store procedure

--- saturdayclub/Controllers/MsgController.cs
+++ saturdayclub/Controllers/MsgController.cs
@@ -6,7 +6,6 @@ using System.Web.Mvc;
 using System.Security.Cryptography;
 using System.Text;
 using saturdayclub.Messages;
-using saturdayclub.Analyze;
 using System.Threading;
 using System.Diagnostics;
 using System.Net;
@@ -22,42 +21,7 @@ namespace saturdayclub.Controllers
         [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Test()
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Restart();
-            string replyMsg = string.Empty;
-            try
-            {
-                using (var context = new AnalyzeContext())
-                {
-                    ActivityAnalyzer analyzer = new ActivityAnalyzer();
-                    AnalyzeToken token = new AnalyzeToken();
-                    using (ManualResetEvent evt = new ManualResetEvent(false))
-                    {
-                        context.RunAsync(() =>
-                        {
-                            token = analyzer.Analyze(null);
-                            evt.Set();
-                        });
-                        if (!evt.WaitOne(20000))
-                        {
-                            throw new TimeoutException("Cannot start new thread!");
-                        }
-                        var result = analyzer.RetrieveResult(token, 20000) as List<string>;
-                        replyMsg = string.Join("; ", result.ToArray());
-                    }
-                    context.Exit();
-                }
-            }
-            catch (Exception ex)
-            {
-                replyMsg = ex.ToString();
-            }
-            if (string.IsNullOrEmpty(replyMsg))
-            {
-                replyMsg = "No activity.";
-            }
-            sw.Stop();
-            return Content(replyMsg + " " + sw.ElapsedMilliseconds + " ms.");
+            return View("test");
         }
 
         [ActionName("webclient")]
@@ -66,53 +30,6 @@ namespace saturdayclub.Controllers
         {
             Stopwatch sw = new Stopwatch();
             sw.Restart();
-            string replyMsg = string.Empty;
-            using (WebClient wc = new WebClient())
-            {
-                wc.Encoding = Encoding.GetEncoding("GBK");
-                replyMsg = wc.DownloadString("http://www.niwota.com/quan/13142806");
-                //replyMsg = Server.HtmlEncode(replyMsg);
-
-                List<string> activityList = new List<string>();
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(replyMsg);
-                var root = doc.GetElementbyId("activities");
-                var table = root.SelectNodes("//div[@class='col_body']/ul[@class='activities txt_light']/li[@class!='head']");
-                foreach (var col in table)
-                {
-                    HtmlNode temp = HtmlNode.CreateNode(col.OuterHtml);
-                    var node = temp.SelectSingleNode("//li/span[@class='theme']/a");
-                    string title = node.InnerText.Trim();
-                    if (!string.IsNullOrEmpty(title))
-                    {
-                        activityList.Add(title);
-                    }
-                }
-                replyMsg = string.Join("; ", activityList.ToArray());
-            }
-            sw.Stop();
-            return Content(sw.ElapsedMilliseconds + " ms.   " + replyMsg);
-        }
-
-        //
-        // GET: /Msg/
-
-        [ActionName("valid")]
-        [AcceptVerbs(HttpVerbs.Get)]
-        public ActionResult Valid(string signature, string timestamp, string nonce, string echostr)
-        {
-            ActionResult result = Content("Please visit via your WeiXin client.");
-            if (MessageValidator.ValidateChecksum(signature, MessageToken, timestamp, nonce))
-            {
-                result = Content(echostr);
-            }
-            return result;
-        }
-
-        [ActionName("trans")]
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult TranslateMessage()
-        {
             string replyMsg = string.Empty;
             try
             {
@@ -140,10 +57,35 @@ namespace saturdayclub.Controllers
                     replyMsg = string.Join("; ", activityList.ToArray());
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 replyMsg = ex.ToString();
             }
+            HttpContext.Application["Activities"] = replyMsg;
+            sw.Stop();
+            return Content(sw.ElapsedMilliseconds + " ms.   " + replyMsg);
+        }
+
+        //
+        // GET: /Msg/
+
+        [ActionName("valid")]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Valid(string signature, string timestamp, string nonce, string echostr)
+        {
+            ActionResult result = Content("Please visit via your WeiXin client.");
+            if (MessageValidator.ValidateChecksum(signature, MessageToken, timestamp, nonce))
+            {
+                result = Content(echostr);
+            }
+            return result;
+        }
+
+        [ActionName("trans")]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult TranslateMessage()
+        {
+            string replyMsg = HttpContext.Application["Activities"] as string;
             if (string.IsNullOrEmpty(replyMsg))
             {
                 replyMsg = "No activity.";
